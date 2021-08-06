2d94182626650119b67add3bf1373023b007f76d
Cloud Yu <j.cloud.yu@gmail.com>
2021-08-07 00:38:45 +0800

    Minor modifications

--- reroot.d.ts
+++ reroot.d.ts
@@ -1,8 +1,12 @@
-/** Assign the path to where `/` starts from. */
-export const root_path:string;
+declare class reroot {
+	/** Where the path '/' starts from when user requires a module using absolute path. */
+	root_path:string;
 
-/** Generates a require function that will returns undefined when the path is invalid. */
-export function safe_require(require:NodeRequire):NodeRequire;
+	/** Generates a require function that will returns undefined when the path is invalid. */
+	safe_require(require:NodeRequire):NodeRequire;
 
-/** Converts the given path to an absolute path according to `root_path` property. */
-export function resolve_path(path:string):string;
+	/** Converts the given path to an absolute path according to `root_path` property. */
+	resolve_path(path:string):string;
+}
+
+export = reroot;
