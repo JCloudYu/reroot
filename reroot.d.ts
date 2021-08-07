70dee97a90d7b03bca637c2e8ffd76a11aea51ed
Cloud Yu <j.cloud.yu@gmail.com>
2021-08-07 12:06:30 +0800

    Fix incorrect property scope

--- reroot.d.ts
+++ reroot.d.ts
@@ -1,12 +1,12 @@
 declare class reroot {
 	/** Where the path '/' starts from when user requires a module using absolute path. */
-	root_path:string;
+	static root_path:string;
 
 	/** Generates a require function that will returns undefined when the path is invalid. */
-	safe_require(require:NodeRequire):NodeRequire;
+	static safe_require(require:NodeRequire):NodeRequire;
 
 	/** Converts the given path to an absolute path according to `root_path` property. */
-	resolve_path(path:string):string;
+	static resolve_path(path:string):string;
 }
 
 export = reroot;
